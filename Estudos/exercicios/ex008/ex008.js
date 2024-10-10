955a1ad68ffc8f7d59e9b0045c67179cf42a7058
lucodego <lucascostadegodoi@gmail.com>
2024-10-10 14:18:58 -0300

    inicio de codigos js puro

---
+++ Estudos/exercicios/ex008/ex008.js
@@ -0,0 +1,9 @@
+var vel = 70
+console.log(`A velocidade do carro é ${vel}`)
+if(vel >= 60){ //condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}else{console.log('Dirija sempre usando cinto de segurança')}
+
+
+var país = 'EUA'
+consol
