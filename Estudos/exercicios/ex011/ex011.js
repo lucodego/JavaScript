f64965a31fff3729e49b302bfa6e6b5b0ccfc853
lucodego <lucascostadegodoi@gmail.com>
2024-10-16 16:59:33 -0300

    Condições multiplas

--- Estudos/exercicios/ex011/ex011.js
+++ Estudos/exercicios/ex011/ex011.js
@@ -1,10 +1,8 @@
-var idade = 122
+var idade = 64
 if (idade < 16){
     console.log('Não vota')
-} else{
-    if(idade <= 18){
+} else if(idade < 18 || idade > 65){
         console.log('Voto opcional')
-    } else{
-        console.log('Vota')
-    }
+} else{
+    console.log('Voto obrigatório')
 }
