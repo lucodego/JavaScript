c7041e8933291532172b23ac7bd91b8791ac60d8
lucodego <lucascostadegodoi@gmail.com>
2024-10-16 12:08:45 -0300

    Create ex011.js

---
+++ Estudos/exercicios/ex011/ex011.js
@@ -0,0 +1,10 @@
+var idade = 122
+if (idade < 16){
+    console.log('Não vota')
+} else{
+    if(idade <= 18){
+        console.log('Voto opcional')
+    } else{
+        console.log('Vota')
+    }
+}
