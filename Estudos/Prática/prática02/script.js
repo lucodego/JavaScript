451a2cda91887ffb3a45f6c48f98d4f682cfbba6
lucodego <lucascostadegodoi@gmail.com>
2024-10-21 12:41:02 -0300

    inicio projeto 2

---
+++ Estudos/Prática/prática02/script.js
@@ -0,0 +1,12 @@
+function verificar() {
+    var agora = new Date()
+    var anoatual = agora.getFullYear()
+    var ano = document.getElementById('ano')
+    var res = document.getElementById('res')
+
+    if (ano.value.length == 0 || ano.value > anoatual){
+        window.alert('Verifique os dados e tente novamente')
+    } else {
+        var 
+    }
+}
