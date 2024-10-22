b0fe4f13e3b5c4d84be27ed83346ae4e223d5930
lucodego <lucascostadegodoi@gmail.com>
2024-10-22 10:04:45 -0300

    finalizado prática 2

--- Estudos/Prática/prática02/script.js
+++ Estudos/Prática/prática02/script.js
@@ -7,6 +7,44 @@ function verificar() {
     if (ano.value.length == 0 || ano.value > anoatual){
         window.alert('Verifique os dados e tente novamente')
     } else {
-        var 
+        var fsex = document.getElementsByName('sexo')
+        var idade = anoatual - Number(ano.value)
+        var gen = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+            if (fsex[0].checked){
+                gen = 'Homem'
+                if(idade >= 0 && idade < 10){
+                    //criança
+                    img.setAttribute('src', 'cri-m.png')
+                } else if (idade < 20){
+                    //jovem
+                    img.setAttribute('src', 'ado-m.png')
+                } else if(idade < 50){
+                    //adulto
+                    img.setAttribute('src', 'adu-m.png')
+                } else {
+                    //idoso
+                    img.setAttribute('src', 'idos-m.png')
+                }
+            }else if(fsex[1].checked) {
+                gen = 'Mulher'
+                if(idade > 0 && idade < 10){
+                    //criança
+                    img.setAttribute('src', 'cri-f.png')
+                } else if (idade < 20){
+                    //jovem
+                    img.setAttribute('src', 'ado-f.png')
+                } else if(idade < 50){
+                    //adulto
+                    img.setAttribute('src', 'adu.png')
+                } else {
+                    //idoso
+                    img.setAttribute('src', 'idos-f.png')
+                }
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gen} com ${idade} anos`
+        res.appendChild(img)
     }
-}
