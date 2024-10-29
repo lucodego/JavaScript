28e1263459d0720e915e531bb7538d3d78208459
lucodego <lucascostadegodoi@gmail.com>
2024-10-28 21:13:19 -0300

    Update script.js

--- Estudos/Prática/prática05/script.js
+++ Estudos/Prática/prática05/script.js
@@ -20,17 +20,42 @@ function inLista(n, l){
 }
 
 function analisar(){
-    if(isNumero(num.value) || inLista(num.value, vetor)){
+    if(isNumero(num.value) && !inLista(num.value, vetor)){
         vetor.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
         tab.appendChild(item)
-
+        res.innerHTML = ''
     } else{
         alert('Valor inválido ou ja encontrado na lista!!')
     }
+    num.value = ''
+    num.focus()
 }
 
+
+
 function finalizar(){
-    
+    if (vetor.length == 0){
+        alert('Adicione valores para analisar')
+    }
+    else{
+        let tot = vetor.length
+        let maior = vetor[0]
+        let menor = vetor[0]
+        for ( let pos in vetor){
+            if (vetor[pos] > maior){
+                maior = vetor[pos]
+            }
+            if (vetor[pos] < menor){
+                menor = vetor[pos]
+            }
+        }
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O menor valor é ${menor}</p>`
+        res.innerHTML += `<p>O maior valor é ${maior}</p>`
+        
+    }
 }
